67f6382e1f1bb1324e0600a041d38fd93efb98d2
Yasin Ekinci <yasin.ekinci@dzdtech.com>
2025-05-11 21:38:38 +0300

    test

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 var summaries = new[]
-{
+{ 
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
