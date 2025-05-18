5f9f4ccd73e8caf8cd381fd988ffce04b157de9b
Yasin Ekinci <yasin.ekinci@dzdtech.com>
2025-05-18 15:40:56 +0300

    add controller and swagger

--- src/aggregator/Aggregator.API/Program.cs
+++ src/aggregator/Aggregator.API/Program.cs
@@ -1,23 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
